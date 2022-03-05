d70b619e24c87957214f23f5f09317fee4f31854
Mendezg1 <eurocoparich@gmail.com>
2022-03-04 18:34:21 -0600

    Cambios

--- StackFactory.java
+++ StackFactory.java
@@ -1,13 +1,13 @@
 public class StackFactory<T>  {
     
-    public Stack<T> Create(String type){
-        if(type.equals("AL")){
+    public Stack<T> Create(int type){
+        if(type == 1){
            return new ALStack<T>();
         }
-        else if(type.equals("V")){
+        else if(type == 2){
             return new VStack<T>();
         }
-        else if(type.equals("L")){
+        else if(type == 3){
             return new LStack<T>();
         }
         else{
