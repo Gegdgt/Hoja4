83a541eff2b732f9a9a59ee2ca05f62e5e3295b5
DC1823 <cue21965@uvg.edu.gt>
2022-03-04 23:48:31 -0600

    cambios

--- Conversor.java
+++ Conversor.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class Conversor {
     public Conversor(int tipo){
         operaciones = SF.Create(tipo);
+        leer();
     }
     public Stack<String> operaciones;
     StackFactory<String> SF = new StackFactory<String>();
